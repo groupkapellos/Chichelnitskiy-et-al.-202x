f204fa7224962b07ff2e9aac901540efd7db47cf
Theodore S. Kapellos <39677623+tkapello@users.noreply.github.com>
2026-05-12 14:28:32 +0200

    DE gene calculation.R

--- DE gene calculation.R
+++ DE gene calculation.R
@@ -1,3 +1,6 @@
+# Remove existing variables from R memory
+rm(list=ls())
+
 # Load packages
 library(Seurat)
 library(dplyr)
@@ -10,22 +13,22 @@ library(pheatmap)
 library(RColorBrewer)
 library(limma)
 library(IHW)
-library(GSVA)
-library(car)
-library(dunn.test)
+
+# Set directory
+setwd('C:/Users/theodoros.kapellos/Desktop')
 
 # Aggregate counts
-Idents(macs)<-'disease'
-macs<-subset(macs, idents=c('Emphysema', 'Fibrosis', 'Tu.free'))
+Idents(mast)<-'disease'
+mast<-subset(mast, idents=c('Emphysema', 'Fibrosis', 'Tu.free'))
 
-table(macs$integrated_snn_res.0.2, macs$LuT)
+table(mast$integrated_snn_res.0.2, mast$LuT)
 
-Idents(macs)<-macs$integrated_snn_res.0.2
+Idents(mast)<-mast$integrated_snn_res.0.2
 final_list<-list()
 
-for(i in levels(macs$integrated_snn_res.0.2)){
+for(i in levels(mast$integrated_snn_res.0.2)){
   
-  tmp<-subset(macs, idents=i)
+  tmp<-subset(mast, idents=i)
   keep_lut<-names(which(table(tmp$LuT) >= 3))
   if(length(keep_lut) < 1) next
   
@@ -38,7 +41,7 @@ for(i in levels(macs$integrated_snn_res.0.2)){
   
   pseudobulk<-pseudobulk$RNA
   pseudobulk<-as.data.frame(pseudobulk)
-  
+
   metadata<-data.frame(patient=colnames(pseudobulk))
   metadata$disease<-'Fibrosis'
   idx<-metadata$patient %in% c('LuT_314','LuT_349','LuT_383','LuT_410','LuT_433','LuT_736')
@@ -47,19 +50,18 @@ for(i in levels(macs$integrated_snn_res.0.2)){
   if(any(idx)) metadata$disease[idx]<-'Control'
   rownames(metadata)<-colnames(pseudobulk)
   
-  if(any(table(factor(metadata$disease, levels=c("Emphysema", "Fibrosis", "Control"))) < 3)) next
+  if(any(table(factor(metadata$disease, levels=c("Emphysema", "Fibrosis"))) < 3)) next  
   
   dds<-DESeqDataSetFromMatrix(countData=pseudobulk,
                               colData=metadata,
                               design=~disease)
-  
-  keep<-rowSums(counts(dds) >= 10) >= 3
+
+  keep<-rowSums(counts(dds)) >= 5 
   dds<-dds[keep,]
   dds<-DESeq(dds)
   
   res.emph<-results(dds,
-                    contrast=list("disease_Emphysema_vs_Control","disease_Fibrosis_vs_Control"),
-                    listValues=c(1, -0.5),
+                    contrast=c("disease", "Emphysema", "Fibrosis"),
                     lfcThreshold=0,
                     alpha=0.1,
                     filterFun=ihw,
@@ -72,12 +74,11 @@ for(i in levels(macs$integrated_snn_res.0.2)){
   if(dim(res.emph)[1]>0){
     res.emph$gene<-rownames(res.emph)
     res.emph$direction<-'Emphysema vs rest'
-    res.emph$cluster<-paste0('macs', i)
+    res.emph$cluster<-paste0('mast', i)
   }
   
   res.fib<-results(dds,
-                   contrast=list("disease_Fibrosis_vs_Control","disease_Emphysema_vs_Control"),
-                   listValues=c(1, -0.5),
+                   contrast=c("disease", "Fibrosis", "Emphysema"),
                    lfcThreshold=0,
                    alpha=0.1,
                    filterFun=ihw,
@@ -90,7 +91,7 @@ for(i in levels(macs$integrated_snn_res.0.2)){
   if(dim(res.fib)[1]>0){
     res.fib$gene<-rownames(res.fib)
     res.fib$direction<-'Fibrosis vs rest'
-    res.fib$cluster<-paste0('macs', i)
+    res.fib$cluster<-paste0('mast', i)
   }
   
   tmp2<-rbind(res.emph, res.fib)
@@ -98,5 +99,5 @@ for(i in levels(macs$integrated_snn_res.0.2)){
 }
 
 df<-do.call(rbind, final_list)
-
-write.csv(df, 'macs_pare.csv')
+    
+write.csv(df, 'mast_pare.csv')
